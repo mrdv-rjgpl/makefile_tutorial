df6a4b79d6b68eba2cd229879ad839323093c856
Mardava Gubbi <mardava.gubbi@teamindus.in>
2017-07-28 08:19:57 +0530

    Add makefile with clean recipe

---
+++ 02CompileAndClean/add_two_numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int AddTwoNumbers(int a, int b)
+{
+	return a + b;
+}
+
+int main(int argc, char **argv)
+{
+	cout << AddTwoNumbers(1, 2) << endl;
+
+	return 0;
+}
