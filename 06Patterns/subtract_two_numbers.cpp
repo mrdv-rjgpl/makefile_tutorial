233aaa7609289fd631a120f226560b69acfd6e25
Mardava Gubbi <mardava.gubbi@teamindus.in>
2017-07-29 09:19:10 +0530

    Add makefile with pattern based recipe

---
+++ 06Patterns/subtract_two_numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int SubtractTwoNumbers(int a, int b)
+{
+	return a - b;
+}
+
+int main(int argc, char **argv)
+{
+	cout << SubtractTwoNumbers(1, 2) << endl;
+
+	return 0;
+}
