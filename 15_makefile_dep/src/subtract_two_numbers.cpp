dc7eb4364041274006c1fcfd5ce8a962dc6d5d4e
Mardava Gubbi <mrdv.rjgpl@gmail.com>
2017-10-15 17:18:54 +0530

    Add makefile dependency generation tutorial
    Empty makefile.dep file created for now. Will be filled in subsequent
    tutorials.

---
+++ 15_makefile_dep/src/subtract_two_numbers.cpp
@@ -0,0 +1,15 @@
+#include "../include/subtract_two_numbers.hpp"
+
+using namespace std;
+
+int SubtractTwoNumbers(int a, int b)
+{
+	return a - b;
+}
+
+int main(int argc, char **argv)
+{
+	cout << SubtractTwoNumbers(1, 2) << endl;
+
+	return 0;
+}
