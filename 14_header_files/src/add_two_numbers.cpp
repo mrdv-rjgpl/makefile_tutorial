fd2f9e886ec48fe3f4bfbf1f91791add17fb6fc0
Mardava Gubbi <mrdv.rjgpl@gmail.com>
2017-10-15 17:06:44 +0530

    Add header file tutorial

---
+++ 14_header_files/src/add_two_numbers.cpp
@@ -0,0 +1,15 @@
+#include "../include/add_two_numbers.hpp"
+
+using namespace std;
+
+int AddTwoNumbers(int a, int b)
+{
+	return a + b;
+}
+
+int main(int argc, char **argv)
+{
+	cout << AddTwoNumbers(1, 2) << endl;
+
+	return 0;
+}
